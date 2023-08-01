380e169fd9bffb6c3a720ee3a2f9cb9e5eacc5e9
originRing <yongyue_geng@163.com>
2023-08-01 11:57:20 +0800

    fix: export util

--- projects/ng-antd-color-picker/src/public-api.ts
+++ projects/ng-antd-color-picker/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/interfaces/type';
 export * from './lib/interfaces/color';
+export * from './lib/util/util';
 export * from './lib/ng-antd-color-picker.component';
 export * from './lib/ng-antd-color-block.component';
 export * from './lib/ng-antd-color-picker.module';
