885a5f4c2dffc17e8a961ad41f5d59b3784dabf0
originRing <48777107+OriginRing@users.noreply.github.com>
2023-11-15 10:56:24 +0800

    feat: add module component in version@17 (#3)

--- projects/ng-antd-color-picker/src/public-api.ts
+++ projects/ng-antd-color-picker/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/interfaces/color';
 export * from './lib/util/util';
 export * from './lib/ng-antd-color-picker.component';
 export * from './lib/ng-antd-color-block.component';
+export * from './lib/ng-antd-color-picker.module';
